aa7adb3612d6400aba59f0f0c2871322820cad25
Vagner Paixao <vagnner.ti@gmail.com>
2021-07-05 14:03:24 -0300

    Meu primeiro comit do projeto

--- src/main/java/com/vapaixao/personapi/controller/PersonController.java
+++ src/main/java/com/vapaixao/personapi/controller/PersonController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.vapaixao.personapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,6 @@ public class PersonController {
 
     @GetMapping
     public String getBook(){
-        return "API Test";
+        return "API Test!";
     }
 }
